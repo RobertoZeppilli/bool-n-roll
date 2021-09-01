7d78174299974a2121b6523a9f46dd6241b1041b
RobertoZeppilli <robzep171@gmail.com>
2021-09-01 15:32:42 +0200

    add: modifiche css

--- resources/views/admin/welcome.blade.php
+++ resources/views/admin/welcome.blade.php
@@ -16,25 +16,28 @@
                 <i class="fas fa-eye card-image-top fa-4x text-center title-purple"></i>
                 <div class="card-body text-center  ">
                   <h5 class="card-title">Profilo</h5>
+                  <p class="title-purple bg-pale rounded py-2">Qui potrai vedere il tuo profilo, modificarlo o cancellarlo.</p>
                   <a class="btn btn-purple text-white" href="{{ route('admin.musicians.show', $user->musician->id) }}">Mostra</a>
                 </div>
             </div>
         </div>
         <div class="col-xs-6 col-md-4 col-lg-4 mb-4">
             <div class="card shadow p-4">
-                <i class="far fa-address-card card-image-top fa-4x text-center title-indigo"></i>
+                <i class="far fa-address-card card-image-top fa-4x text-center title-pink"></i>
                 <div class="card-body text-center ">
                   <h5 class="card-title">Crea profilo</h5>
-                  <a class="btn btn-indigo text-white" href="{{ route('admin.musicians.create', $user->id) }}">Crea</a>
+                  <p class="title-pink bg-pale rounded py-2">Crea subito il tuo profilo da musicista!</p>
+                  <a class="btn btn-pink text-white" href="{{ route('admin.musicians.create', $user->id) }}">Crea</a>
                 </div>
             </div>
         </div>
         <div class="col-xs-6 col-md-4 col-lg-4 mb-4">
             <div class="card shadow p-4">
-                <i class="far fa-envelope card-image-top fa-4x text-center title-cyan"></i>
+                <i class="far fa-envelope card-image-top fa-4x text-center title-petrol"></i>
                 <div class="card-body text-center ">
                   <h5 class="card-title">Messaggi</h5>
-                  <a class="btn btn-cyan text-white">Messaggi</a>
+                  <p class="bg-pale title-petrol py-2 rounded">Qui troverai tutti i messaggi in entrata.</p>
+                  <a class="btn btn-petrol text-white">Messaggi</a>
                 </div>
             </div>
         </div>
@@ -43,25 +46,28 @@
                 <i class="fas fa-rocket card-image-top fa-4x text-center title-green"></i>
                 <div class="card-body text-center ">
                   <h5 class="card-title">Sponsorizza</h5>
+                  <p class="bg-pale title-green py-2 rounded">Da qui potrai sponsorizzare il tuo profilo, per renderlo più visibile!</p>
                   <a class="btn btn-green text-white">Sponsorizza</a>
                 </div>
             </div>
         </div>
         <div class="col-xs-6 col-md-4 col-lg-4 mb-4">
             <div class="card shadow p-4">
-                <i class="fas fa-chart-pie card-image-top fa-4x text-center title-red"></i>
+                <i class="fas fa-chart-pie card-image-top fa-4x text-center title-yellow"></i>
                 <div class="card-body text-center ">
                   <h5 class="card-title">Statistiche</h5>
-                  <a class="btn btn-red text-white">Statistiche</a>
+                  <p class="title-yellow bg-pale rounded py-2">Qui potrai dare un'occhiata alle statistiche del tuo profilo.</p>
+                  <a class="btn btn-yellow text-white">Statistiche</a>
                 </div>
             </div>
         </div>
         <div class="col-xs-6 col-md-4 col-lg-4 mb-4">
             <div class="card shadow p-4">
-                <i class="far fa-comments card-image-top fa-4x text-center title-teal"></i>
+                <i class="far fa-comments card-image-top fa-4x text-center title-wine"></i>
                 <div class="card-body text-center ">
                   <h5 class="card-title">Recensioni</h5>
-                  <a class="btn btn-teal text-white">Recensioni</a>
+                  <p class="title-wine rounded bg-pale py-2">Qui potrai leggere le recensioni su di te.</p>
+                  <a class="btn btn-wine text-white">Recensioni</a>
                 </div>
             </div>
         </div>
