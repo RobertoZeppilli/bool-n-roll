0c32866e25a7d8954c6316f7aa5f1bbf5d7146c0
michelafranchini <michela.franchini.91@gmail.com>
2021-09-10 15:47:34 +0200

    add: message/review confirm

--- resources/js/pages/MessageConfirm.vue
+++ resources/js/pages/MessageConfirm.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>
-      Messaggio inviato
+  <div class="container d-flex align-items-center justify-content-center">
+    <div class="card text-center d-flex align-items-center justify-content-center p-5 card_review shadow p-3 mb-2 bg-body rounded">
+      <div class="thumbs_container d-flex align-items-center justify-content-center">
+        <i class="fas fa-thumbs-up title-orange"></i>
+      </div>
 
-      <a class="btn btn-primary text-white" @click="$router.go(-1)">
+      <h1 class="my-5">Messaggio inviato correttamente!</h1>
+      <p>Il musicista ha ricevuto il tuo messaggio e ti risponderà nel più breve tempo possibile</p>
+
+      <a class="btn btn-orange text-white my-3" @click="$router.go(-1)">
         Indietro
-      </a>
+      </a>      
+    </div>
   </div>
 </template>
 
@@ -14,6 +21,21 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+  .container {
+    height: calc(100vh - 76px);
+
+    .thumbs_container {
+      border-radius:150px;
+      height:150px; 
+      width:150px;
+      background-color: #c6d8d3;
+
+      .fa-thumbs-up {
+        font-size: 50px;
+      }
+    }
+  }
+  
 
 </style>
